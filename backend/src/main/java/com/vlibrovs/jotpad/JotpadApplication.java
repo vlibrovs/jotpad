f4f6f2de442380ba14d26b4c6fe4dbe02f5a4481
Vladyslav Librovskyi <vlad.librovskyi@gmail.com>
2024-02-08 19:35:33 +0300

    Initial commit

---
+++ backend/src/main/java/com/vlibrovs/jotpad/JotpadApplication.java
@@ -0,0 +1,13 @@
+package com.vlibrovs.jotpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotpadApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vlibrovs/jotpad/JotpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlibrovs.jotpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
